407445b31c26375606e5822e3b442df3f9f9cc60
BlueBatDevil <bluebatriaw@gmail.com>
2023-08-21 17:29:49 +0000

    project has been initialized

---
+++ src/test/java/ru/eienting/sds/SdsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eienting.sds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
